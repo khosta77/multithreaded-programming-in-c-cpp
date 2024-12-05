6e23ec8fb9dce725976ddb47f721b30c6cce2868
khosta77 <puwerfulpants@gmail.com>
2024-12-05 18:12:38 +0300

    Запускается

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: clean main threadpool
 
 main:
-	g++ -pthread -std=c++17 threadpool.cpp -o threadpool.out
+	g++ -pthread -std=c++20 threadpool.cpp -o threadpool.out
 
 clean:
 	rm -rf *.out
--- threadpool.cpp
+++ threadpool.cpp
@@ -11,6 +11,11 @@
 #include <exception>
 #include <future>
 #include <iomanip>
+#include <type_traits>
+#include <functional>
+
+template<typename Func, typename... Args>
+using result_of_t = std::invoke_result_t<Func, Args...>;
 
 class ThreadPool
 {
@@ -43,14 +48,39 @@ public:
         }
     }
 
-    bool AddTask( std::function<void()> f )
+    //template<typename R = void>
+    //std::future<R>  // Получает результат
+    //AddTask( std::function<R()> f )
+
+    template<typename T>
+    std::future<result_of_t<T>>  // Получает результат
+    AddTask( T&& f )
     {
+        using R = result_of_t<T>;
+        // std::promise<R> promise;  // Позволяет создать результат
         std::unique_lock<std::mutex> lock(mutex_);
         if( state_ != State::OK )
-            return false;
-        tasks_.push_back(f);
+            return std::future<R>();
+        
+
+        std::promise<R> prom;
+        std::future<R> res = prom.get_future();
+
+        tasks_.emplace_back(
+                    [ f = std::move(f), p = std::move(prom) ]() mutable
+                    {
+                        try
+                        {
+                            p.set_value( f() );
+                        }
+                        catch( ... )
+                        {
+                            p.set_exception( std::current_exception() );
+                        };
+                    }
+                );
         cond_var_.notify_one();
-        return true;
+        return res;
     }
 
     void Stop()
@@ -63,6 +93,8 @@ public:
         cond_var_.notify_all();
     }
 
+    // можно извлечь ошибку в отдельной функции, надо не забыть mutex
+
 private:
     void ThreadFunction()
     {
@@ -73,23 +105,24 @@ private:
                         && state_ != State::OK ); } );
             while( !tasks_.empty() )
             {
-                std::function<void()> f = std::move(tasks_.front());
+                // std::function<void()> f = std::move(tasks_.front());
+                std::packaged_task<void()> f = std::move(tasks_.front());
+
                 tasks_.pop_front();
 
                 lock.unlock();
+                f();
+/*
                 try
                 {
                     f();
                 }
-                catch( std::exception& exc )
-                {
-                    std::cerr << "!> Exception! " << exc.what() << std::endl;
-                    // Надо в метрику записывать
-                }
                 catch( ... )
                 {
-                    std::cerr << "!> Exception! Unknown!" << std::endl;
+                    eptr_ = std::current_exception();
                 };
+*/
+
                 lock.lock();
             }
         }
@@ -101,22 +134,51 @@ private:
 
 private:
     std::vector<std::thread> threads_;
-    std::deque<std::function<void()>> tasks_;
+    std::deque<std::packaged_task<void()>> tasks_;
 
     size_t count_active_;
 
     State state_;
     std::mutex mutex_;
     std::condition_variable cond_var_;
+    
+    std::exception_ptr eptr_;  // ...vector...
 };
+
+bool task()
+{
+    int x = 1;
+    while(x)
+    {
+        x -= 1; 
+    }
+    return true;
+}
 
 int main()
 {
-    const size_t I = 8;
+    const size_t I = 4;
     ThreadPool p(I);
     for( size_t i = 0; i < I; ++i )
     {
-        std::cout << i << ") " <<  std::boolalpha << p.AddTask([](){while(1) { 2 + 3 / 5 * 10;}}) << std::endl;
+        p.AddTask([]() -> bool { return task(); });
     }
+
+    std::cout << "next" << std::endl;
+    auto res = p.AddTask([]() -> int { return ( task() ? 2 : 3 ); });
+    if( !res.valid() )
+    {
+        std::cerr << "..." << std::endl;
+    }
+    else
+    {
+        std::cout << "success\n";
+        res.wait();
+        std::cout << "wait\n";
+        res.get();
+        std::cout << "get\n";
+    }
+    p.Stop();
+    std::cout << "stop\n";
     return 0;
 }
